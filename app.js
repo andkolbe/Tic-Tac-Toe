23ff947e0d2705f5213212a5ce399b1f73a1a981
Andrew Kolbe <andrewkolbe@Erins-MacBook-Pro.local>
2020-10-01 02:51:34 -0400

    Complete

--- app.js
+++ app.js
@@ -16,47 +16,52 @@ function cellClicked(event) {
     }
     player = !player;
 
-    if (cells[0].textContent === 'X' && cells[1].textContent === 'X' && cells[2].textContent === 'X'
+    const xWins = (cells[0].textContent === 'X' && cells[1].textContent === 'X' && cells[2].textContent === 'X'
         || cells[3].textContent === 'X' && cells[4].textContent === 'X' && cells[5].textContent === 'X'
         || cells[6].textContent === 'X' && cells[7].textContent === 'X' && cells[8].textContent === 'X'
         || cells[0].textContent === 'X' && cells[3].textContent === 'X' && cells[6].textContent === 'X'
         || cells[1].textContent === 'X' && cells[4].textContent === 'X' && cells[7].textContent === 'X'
         || cells[2].textContent === 'X' && cells[5].textContent === 'X' && cells[8].textContent === 'X'
         || cells[0].textContent === 'X' && cells[4].textContent === 'X' && cells[8].textContent === 'X'
-        || cells[2].textContent === 'X' && cells[4].textContent === 'X' && cells[6].textContent === 'X'
-        || cells[0].textContent === 'O' && cells[1].textContent === 'O' && cells[2].textContent === 'O'
+        || cells[2].textContent === 'X' && cells[4].textContent === 'X' && cells[6].textContent === 'X');
+
+    const oWins = (cells[0].textContent === 'O' && cells[1].textContent === 'O' && cells[2].textContent === 'O'
         || cells[3].textContent === 'O' && cells[4].textContent === 'O' && cells[5].textContent === 'O'
         || cells[6].textContent === 'O' && cells[7].textContent === 'O' && cells[8].textContent === 'O'
         || cells[0].textContent === 'O' && cells[3].textContent === 'O' && cells[6].textContent === 'O'
         || cells[1].textContent === 'O' && cells[4].textContent === 'O' && cells[7].textContent === 'O'
         || cells[2].textContent === 'O' && cells[5].textContent === 'O' && cells[8].textContent === 'O'
         || cells[0].textContent === 'O' && cells[4].textContent === 'O' && cells[8].textContent === 'O'
-        || cells[2].textContent === 'O' && cells[4].textContent === 'O' && cells[6].textContent === 'O') {
-        console.log('You Win!');
-        restart();
-        
+        || cells[2].textContent === 'O' && cells[4].textContent === 'O' && cells[6].textContent === 'O');
+
+    if (xWins) {
+        document.getElementById('winner').innerHTML = 'X Wins!';
+    } if (oWins) {
+        document.getElementById('winner').innerHTML = 'O Wins!';
     }
 
-    moveCount++
+    if (xWins && event.target.textContent === 'O') {
+        reload();
+    } if (oWins && event.target.textContent === 'X') {
+        reload();
+    }
+
+    moveCount++;
     if (moveCount === 9) {
-        console.log('No One Wins!'); 
+        document.getElementById('winner').innerHTML = ('No One Wins!');
     } else if (moveCount === 10) {
-        restart();
+        reload();
     }
 
-
-
-
-
-    function restart() {
+    function reload() {
         location.reload();
     }
-
-
-
 
 };
 
 
 
 
+
+
+ 
